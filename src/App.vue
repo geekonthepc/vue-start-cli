32832ac959dd4ae65b18febc532a935d1a1148ef
Alex Ward <alex@alexward.co.uk>
2023-05-14 14:42:13 +0100

    Load menu items from json

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,12 @@
         :nav-link-click="(index) => activePage = index"
     ></navbar>    
 
+    <div v-show="false">
+        hide this content
+    </div>
+
     <page-viewer 
+        v-if="pages.length > 0"
         :page="pages[activePage]"
     ></page-viewer>
 </template>
@@ -19,27 +24,22 @@ export default {
             Navbar,
             PageViewer
         },
+        created() {
+            this.getPages();
+        },
         data() {
             return {
                 activePage: 0,
-                pages: [
-                    {
-                        link: {text: 'Home', url: 'index.html'},
-                        pageTitle: 'Home Page',
-                        content: 'This is the home content'
-                    },
-                    {
-                        link: {text: 'About', url: 'about.html'},
-                        pageTitle: 'About',
-                        content: 'This is the about content'
-                    },
-                    {
-                        link: {text: 'Contact', url: 'contact.html'},
-                        pageTitle: 'Contact',
-                        content: 'This is the contact content'
-                    }
-                ]
+                pages: []
             };
+        },
+        methods: {
+            async getPages() {
+                let res = await fetch('pages.json');
+                let data = await res.json();
+
+                this.pages = data;
+            }
         }
     }
 </script>
